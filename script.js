3d51253b15e291cb504ac56394ae092ff7305e25
Martin Filo <martinxfilo@gmail.com>
2018-12-29 18:25:05 +0100

    page navigation

---
+++ script.js
@@ -0,0 +1,17 @@
+const hidePages = () => {
+    $('.single_page').addClass('hidden')
+};
+
+const showPage = (id) => {
+    hidePages();
+    $(id).removeClass('hidden')
+};
+
+(()=>{
+    hidePages();
+    showPage('#page_main')
+})();
+
+window.onhashchange = function (e) {
+    showPage('#' + e.newURL.split('#')[1])
+};
