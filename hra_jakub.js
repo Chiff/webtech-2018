55f652b74c82df5d2a53158cd37c3e078bc5fda8
Jakub <xjadvis@stuba.sk>
2019-01-04 19:19:34 +0100

    update hra

--- hra_jakub.js
+++ hra_jakub.js
@@ -1,6 +1,25 @@
-/**
- * toto sa ti pusti po nacitanin script.js automaticky
- */
+import Stopwatch from "./stop_watch.js"
 export default function () {
     console.log('hra jakub')
 }
+
+window.allowDrop = function (ev) {
+    ev.preventDefault();
+}
+
+window.drag = function(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+window.drop=function (ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+
+window.stopwatch = new Stopwatch(
+    document.querySelector('.stopwatch'),
+    document.querySelector('.results')
+);
+
---
+++ stop_watch.js
@@ -0,0 +1,92 @@
+export default class Stopwatch {
+
+    constructor(display, results) {
+        this.running = false;
+        this.display = display;
+        this.results = results;
+        this.reset();
+        this.print(this.times);
+        hidePieces();
+    }
+
+    reset() {
+        this.times = [0, 0, 0];
+    }
+
+    start() {
+        showPieces();
+        if (!this.time) this.time = performance.now();
+        if (!this.running) {
+            this.running = true;
+            requestAnimationFrame(this.step.bind(this));
+        }
+    }
+
+    stop() {
+        hidePieces();
+        this.running = false;
+        this.time = null;
+    }
+
+    restart() {
+        this.stop();
+        this.times = [0, 0, 0];
+        this.print();
+
+    }
+
+    step(timestamp) {
+        if (!this.running) return;
+        this.calculate(timestamp);
+        this.time = timestamp;
+        this.print();
+        requestAnimationFrame(this.step.bind(this));
+    }
+
+    calculate(timestamp) {
+        var diff = timestamp - this.time;
+        this.times[2] += diff / 10;
+        if (this.times[2] >= 100) {
+            this.times[1] += 1;
+            this.times[2] -= 100;
+        }
+        if (this.times[1] >= 60) {
+            this.times[0] += 1;
+            this.times[1] -= 60;
+        }
+    }
+
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+
+    format(times) {
+        return `\
+${pad0(times[0], 2)}:\
+${pad0(times[1], 2)}:\
+${pad0(Math.floor(times[2]), 2)}`;
+    }
+}
+
+
+function pad0(value, count) {
+    var result = value.toString();
+    for (; result.length < count; --count)
+        result = '0' + result;
+    return result;
+}
+
+function clearChildren(node) {
+    while (node.lastChild)
+        node.removeChild(node.lastChild);
+}
+
+window.hidePieces = function () {
+    $("#myDIV").hide();
+}
+
+window.showPieces = function () {
+    $("#myDIV").show();
+}
+
+
